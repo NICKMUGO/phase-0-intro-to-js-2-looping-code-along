2249e1eec456beb8df6b9bcd22186d86a5aed631
NICK MUGO NJURU <nicknjuru13@gmail.com>
2023-07-15 09:53:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(stringnames,greeting,message,newArray){
+    function convertToStr(Number){
+        let message = "";
+    
+    }
+    for (let i = 0; i < stringnames.length; i++) {
+        stringnames=["Ada", "Brendan", "Ali"];
+        greeting="Thankyou";
+        message=convertToStr(`${greeting},${stringnames[i]}`);
+        newArray=message;
+    }
+        return newArray;
+}
+function countdown(integer){
+    let i =0
+    while (integer<=10){
+        let integer=10;
+        integer--
+        console.log(integer);
+    }
+}
